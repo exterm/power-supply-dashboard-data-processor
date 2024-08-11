76231f8cb09f4814b6d7f3a9373e9300b2eea450
Philip Theus <philip@simplexity.quest>
2024-08-11 18:21:02 -0400

    record historical data in supabase

--- cloud-function/main.py
+++ cloud-function/main.py
@@ -5,6 +5,8 @@ import functions_framework
 
 import supabase
 
+from utils import electricity_maps
+
 SUPABASE_URL = 'https://zsfmcbykdoviifsoauxs.supabase.co'
 SUPABASE_TOKEN = os.getenv('SUPABASE_TOKEN')
 
@@ -12,12 +14,21 @@ SUPABASE_TOKEN = os.getenv('SUPABASE_TOKEN')
 def main(request: flask.Request) -> flask.typing.ResponseReturnValue:
     db: supabase.Client = supabase.create_client(SUPABASE_URL, SUPABASE_TOKEN)
 
-    response = db.table('dashboard-snapshots').select("*").execute()
+    electricity_maps_client = electricity_maps.Client()
 
-    print(response.json())
+    interesting_zones = ['CA-ON', 'US-NE-ISNE']
 
-    response = db.table('dashboard-snapshots').insert({
-      'region': 'Ontario',
-      'data': {"carbon_free_share": 0.5},
-    }).execute()
-    return response.json()
+    for zone in interesting_zones:
+        carbon_intensity = electricity_maps_client.get_carbon_intensity(zone)
+        power_breakdown = electricity_maps_client.get_power_breakdown(zone)
+        db.table('electricitymaps-hourly').insert({
+            'zone': zone,
+            'carbon_intensity_raw': carbon_intensity,
+            'power_breakdown_raw': power_breakdown,
+            'testing': not is_running_in_gcp()
+        }).execute()
+
+    return 'OK'
+
+def is_running_in_gcp():
+    return os.getenv('GCP_PROJECT') is not None
