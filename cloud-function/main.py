90f3b6634561aff7dd947fd3debf9f718beb6ac9
Philip Theus <philip@simplexity.quest>
2024-08-11 15:16:19 -0400

    connect to supabase

--- cloud-function/main.py
+++ cloud-function/main.py
@@ -3,6 +3,12 @@ import os
 import flask
 import functions_framework
 
+import supabase
+
+SUPABASE_URL = 'https://zsfmcbykdoviifsoauxs.supabase.co'
+SUPABASE_TOKEN = os.getenv('SUPABASE_TOKEN')
+
 @functions_framework.http
 def main(request: flask.Request) -> flask.typing.ResponseReturnValue:
-    return os.getenv('SUPABASE_TOKEN')
+    db: supabase.Client = supabase.create_client(SUPABASE_URL, SUPABASE_TOKEN)
+    return "OK"
